cb4f04adc838e304d4a6270c3e979362495f32ab
Maksymilian Zawiła <s25085@pjwstk.edu.pl>
2023-03-08 13:34:38 +0100

    Completed tasks

---
+++ Zadanie2_08-03.py
@@ -0,0 +1,16 @@
+# Maksymilian Zawiła s25085 gr.24c
+
+print("Podaj wiek: ")
+age = input()
+print("Podaj wzrost: ")
+height = input()
+print("Podaj kolor oczu: ")
+eye_color = input()
+print("Podaj kolor wlosow: ")
+hair_color = input()
+
+print("\nDane użytkownika:")
+print("Wiek: ", age)
+print("Wzrost: ", height)
+print("Kolor oczu: ", eye_color)
+print("Kolor wlosow: ", hair_color)
