3d5d2a7ac37e0878c09005f9b259555f40365553
Maksymilian Zawiła <s25085@pjwstk.edu.pl>
2023-03-15 13:39:59 +0100

    Add completed additional tasks

---
+++ Lab02 15.03.2023/Dodatkowe - BinOctHex.py
@@ -0,0 +1,6 @@
+bin = input("Podaj bin: ")
+oct = input("Podaj oct: ")
+hex = input("Podaj hex: ")
+print("Bin: ", int(bin,2))
+print("Oct: ", int(oct,8))
+print("Hex: ", int(hex,16))
---
+++ Lab02 15.03.2023/Dodatkowe - Delta.py
@@ -0,0 +1,11 @@
+a = float(input("Podaj a: "))
+b = float(input("Podaj b: "))
+c = float(input("Podaj c: "))
+delta = b**2-4*a*c
+if delta<0:
+    print("Delta jest mniejsza od zera")
+elif delta>0:
+    print("Delta jest większa od zera")
+else:
+    print("Delta jest równa zero")
+print("Delta:", delta)
